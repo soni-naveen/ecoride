10b28ac1fbc76e6fb4de65d4ce010cbf90cdadf0
Naveen Soni <naveen0929.be21@chitkara.edu.in>
2024-07-07 05:36:36 +0530

    namespace bug in chat section fix

--- src/pages/Chat.jsx
+++ src/pages/Chat.jsx
@@ -47,7 +47,7 @@ function ChatPage() {
   }, [driverId, passengerId]);
 
   useEffect(() => {
-    const newSocket = io(ENDPOINT, {
+    const newSocket = io("https://theecoride.in", {
       transports: ["websocket", "polling"],
       withCredentials: true,
     });
