c9f1262afce4912c6c3038d13c61c9e092694211
Naveen Soni <naveennsonii@gmail.com>
2025-06-06 23:17:33 +0530

    feat: new loader

--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -1,14 +1,8 @@
-import React from "react";
 import Lottie from "lottie-react";
 import loadingAnimation from "../animations/LoadAnimation.json";
 
 const Loader = () => {
-  return (
-    <Lottie
-      animationData={loadingAnimation}
-      className="md:h-48 md:w-48 h-60 w-60"
-    />
-  );
+  return <Lottie animationData={loadingAnimation} />;
 };
 
 export default Loader;
